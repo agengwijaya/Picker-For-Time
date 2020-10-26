436ef566837220f926eceb2e3285b065784f06c8
agengwijaya <agengwijaya003@gmail.com>
2020-10-26 15:50:02 +0700

    Tugas Picker For Time

---
+++ app/src/main/java/com/example/pickerfortime/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pickerfortime;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.timepicker));
+    }
+
+    public void processTimePickerResult(int hour, int minute) {
+        String hour_string = Integer.toString(hour);
+        String minute_string = Integer.toString(minute);
+        String timeMessage = (hour_string + ":" + minute_string);
+        Toast.makeText(this, getString(R.string.time) + timeMessage, Toast.LENGTH_SHORT).show();
+    }
+}
